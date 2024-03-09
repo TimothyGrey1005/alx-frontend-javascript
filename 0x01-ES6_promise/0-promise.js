5e82eea79c8314dc429a3848d9f6d8c135d0aaec
TimothyGrey1005 <omphilegopane913@gmail.com>
2024-03-09 02:42:10 +0000

    Refactored

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,14 @@
-export default function getResponseFromAPI() {
-  const success = true;
+export default function (boolean) {
   return new Promise((resolve, reject) => {
-    if (success) {
-      resolve();
+    const object = {
+      status: 200,
+      body: 'Success',
+    };
+
+    if (boolean === true) {
+      resolve(object);
     } else {
-      reject();
+      reject(Error('The fake API is not working currently'));
     }
   });
 }
