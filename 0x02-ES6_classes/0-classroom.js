68fb6989ab5890ae365f15e4e79a683384b3ad9b
TimothyGrey1005 <omphilegopane913@gmail.com>
2024-03-09 03:04:01 +0000

    update

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,12 @@
+/**
+ * Represent classroom.
+ */
+export default class ClassRoom {
+  /**
+   * Make new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The max number of students in class
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
